4cf00e2a46b70449feafdafc44e0da41bd3cbf14
Nanners05 <aidan05.brown@gmail.com>
2026-02-04 20:31:11 +0000

    homework

---
+++ hw3/hw3.py
@@ -0,0 +1,47 @@
+# 3.4
+
+for i in range(2):
+    for l in range(7):
+        print("@", end="")
+    print()
+
+
+#3.9
+num = input("Please enter a number between 7 and 10 digits >")
+
+for i in num:
+    print(i)
+
+
+# 3.11
+list = []
+list2 = []
+
+while True:
+    miles = float(input("Enter how many miles traveled >"))
+    if miles == 0:
+        print(f"The overall average miles/gallon was: {sum(list)/sum(list2)}")
+        break
+    gas = float(input("Enter amount of gas used >"))
+    list.append(miles)
+    list2.append(gas)
+
+    print(f"The miles/gallon for this tank was {miles/gas}")
+        
+
+# 3.12
+num = int(input("Please input a 5-digit integer >"))
+orig = num
+backwards = 0
+
+while num > 0:
+    first = num % 10
+    backwards = reversed * 10 + first
+    num //= 10
+
+if orig == reversed:
+    print("Palindrome!!")
+else:
+    print("Not a Palindrome")
+
+# 3.14
--- test.py
+++
@@ -1,5 +0,0 @@
-
-print("I really hope it snows today")
-
-for i in range(100):
-    print("Snow")
